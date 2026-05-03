7a383d955fcbfb5356c3f90ebf3946e759bfd873
vaheedgit26 <vaheed.aws@gmail.com>
2026-05-03 09:40:45 +0530

    Delete services/api-gateway directory

--- services/api-gateway/config.py
+++
@@ -1,8 +0,0 @@
-import os
-
-AWS_REGION = os.getenv("AWS_REGION", "ap-south-1")
-USER_POOL_ID = os.getenv("USER_POOL_ID", "your_user_pool_id")
-APP_CLIENT_ID = os.getenv("APP_CLIENT_ID", "your_app_client_id")
-
-COGNITO_ISSUER = f"https://cognito-idp.{AWS_REGION}.amazonaws.com/{USER_POOL_ID}"
-JWKS_URL = f"{COGNITO_ISSUER}/.well-known/jwks.json"
